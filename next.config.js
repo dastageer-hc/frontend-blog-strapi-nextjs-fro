25be5c8239f3cb555ecab3bfcb13d205b180ce4c
Dastageer <dastageerh@Dastageers-MacBook-Air-2.local>
2025-05-09 23:04:11 +0530

    next config tweaked

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: ['funny-acoustics-800f9a702f.strapiapp.com'],
   },
+  experimental: {
+    optimizeFonts: false,
+    webpackBuildWorker: false,
+  },
+  webpack: (config) => {
+    // Optimize webpack build
+    config.optimization = {
+      ...config.optimization,
+      minimize: true,
+      moduleIds: 'deterministic',
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
